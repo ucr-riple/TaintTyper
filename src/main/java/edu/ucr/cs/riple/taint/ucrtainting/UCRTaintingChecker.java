c13398e1b77f2ecf9f682342292c32d256bae9f8
Kanak Das <kdas006@ucr.edu>
2022-11-16 05:21:40 -0800

    Add java.sql Connection astub for prepareStatement related sinks

--- src/main/java/edu/ucr/cs/riple/taint/ucrtainting/UCRTaintingChecker.java
+++ src/main/java/edu/ucr/cs/riple/taint/ucrtainting/UCRTaintingChecker.java
@@ -1,8 +1,12 @@
 package edu.ucr.cs.riple.taint.ucrtainting;
 
 import org.checkerframework.common.basetype.BaseTypeChecker;
+import org.checkerframework.framework.qual.StubFiles;
 
 /**
  * This is the entry point for pluggable type-checking.
  */
+@StubFiles({
+        "Connection.astub",
+})
 public class UCRTaintingChecker extends BaseTypeChecker {}
