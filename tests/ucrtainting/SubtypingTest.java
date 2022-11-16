1bd0afbe42b634333cb427bd50d8b424615dc551
Kanak Das <kdas006@ucr.edu>
2022-11-15 23:58:56 -0800

    Update test cases according to new quals

--- tests/ucrtainting/SubtypingTest.java
+++ tests/ucrtainting/SubtypingTest.java
@@ -2,11 +2,11 @@ import edu.ucr.cs.riple.taint.ucrtainting.qual.*;
 
 // Test basic subtyping relationships for the UCR Tainting Checker.
 class SubtypeTest {
-    void allSubtypingRelationships(@UCRTaintingUnknown int x, @UCRTaintingBottom int y) {
-        @UCRTaintingUnknown int a = x;
-        @UCRTaintingUnknown int b = y;
+    void allSubtypingRelationships(int x, @Untainted int y) {
+        @Tainted int a = x;
+        @Tainted int b = y;
         // :: error: assignment
-        @UCRTaintingBottom int c = x; // expected error on this line
-        @UCRTaintingBottom int d = y;
+        @Untainted int c = x; // expected error on this line
+        @Untainted int d = y;
     }
 }
