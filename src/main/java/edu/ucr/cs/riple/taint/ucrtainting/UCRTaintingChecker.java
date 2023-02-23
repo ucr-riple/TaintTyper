8b4573989a45e8b38f5bcb2a898047fcaf2d6e3e
nimakarimipour <karimipour.nima@gmail.com>
2023-02-23 13:38:02 -0800

    add google java format

--- src/main/java/edu/ucr/cs/riple/taint/ucrtainting/UCRTaintingChecker.java
+++ src/main/java/edu/ucr/cs/riple/taint/ucrtainting/UCRTaintingChecker.java
@@ -3,13 +3,11 @@ package edu.ucr.cs.riple.taint.ucrtainting;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.framework.qual.StubFiles;
 
-/**
- * This is the entry point for pluggable type-checking.
- */
+/** This is the entry point for pluggable type-checking. */
 @StubFiles({
-        "Connection.astub",
-        "apache.commons.io.astub",
-        "apache.commons.lang.astub",
-        "general.astub",
+  "Connection.astub",
+  "apache.commons.io.astub",
+  "apache.commons.lang.astub",
+  "general.astub",
 })
 public class UCRTaintingChecker extends BaseTypeChecker {}
