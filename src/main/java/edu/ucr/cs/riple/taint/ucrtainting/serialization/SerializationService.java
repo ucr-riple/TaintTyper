536dfee449f362e8790b6a31a5de122f8465b58a
nimakarimipour <karimipour.nima@gmail.com>
2023-02-25 14:29:57 -0800

    add sourceForFix method

--- src/main/java/edu/ucr/cs/riple/taint/ucrtainting/serialization/SerializationService.java
+++ src/main/java/edu/ucr/cs/riple/taint/ucrtainting/serialization/SerializationService.java
@@ -4,6 +4,7 @@ import com.sun.source.tree.Tree;
 import com.sun.source.util.TreePath;
 import java.util.HashSet;
 import java.util.Set;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.checkerframework.framework.source.SourceVisitor;
 
 /** This class is used to serialize the errors and the fixes for the errors. */
@@ -37,9 +38,40 @@ public class SerializationService {
     // TODO: for TreeChecker instance below, use the actual API which checks if the tree is
     // @Tainted. For now, we pass tree -> true, to serialize a fix for all expressions on the right
     // hand side of the assignment.
+    Tree sourceForFix = getSourceForFix(source, messageKey);
+    if (sourceForFix == null) {
+      return;
+    }
     Set<Fix> resolvingFixes =
-        generateFixesForExpression((Tree) source, tree -> true, visitor.getCurrentPath());
+        generateFixesForExpression(sourceForFix, tree -> true, visitor.getCurrentPath());
     // TODO: serialize the error and the fixes, will be implemented in the next PR, once the format
     // is finalized.
+  }
+
+  /**
+   * Returns the source for the fix based on the given message key. For inheritance related errors,
+   * the fix requires a change on the parent method, for all others the source itself is the root
+   * for applying fixes.
+   *
+   * @param source the source of the error.
+   * @param messageKey the key of the error message.
+   * @return the source for the fix based on the given message key.
+   */
+  @Nullable
+  private Tree getSourceForFix(Object source, String messageKey) {
+    if (!(source instanceof Tree)) {
+      // For all cases where the source is not a tree, we return null for now.
+      return null;
+    }
+    switch (messageKey) {
+      case "override.param":
+      case "override.return":
+        // TODO: for now, we return null, will be implemented in the next PR which handles
+        // inheritance.
+        return null;
+      default:
+        // For all other cases, the involved tree in the error is the source for fix.
+        return (Tree) source;
+    }
   }
 }
