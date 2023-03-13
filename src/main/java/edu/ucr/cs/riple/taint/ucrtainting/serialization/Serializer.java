4137ac5219e24ab579f81e0507393deacb42d610
nimakarimipour <karimipour.nima@gmail.com>
2023-03-13 12:01:09 -0500

    update serialize symbol logic

--- src/main/java/edu/ucr/cs/riple/taint/ucrtainting/serialization/Serializer.java
+++ src/main/java/edu/ucr/cs/riple/taint/ucrtainting/serialization/Serializer.java
@@ -1,10 +1,15 @@
 package edu.ucr.cs.riple.taint.ucrtainting.serialization;
 
+import static java.util.stream.Collectors.joining;
+
 import com.sun.tools.javac.code.Symbol;
+import com.sun.tools.javac.code.Type;
+import com.sun.tools.javac.util.Name;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import javax.annotation.Nullable;
 
 /**
  * Serializer class where all generated files in Fix Serialization package is created through APIs
@@ -37,11 +42,56 @@ public class Serializer {
    * Serializes the given {@link Symbol} to a string.
    *
    * @param symbol The symbol to serialize.
-   * @param adapter adapter used to serialize symbols.
    * @return The serialized symbol.
    */
-  public static String serializeSymbol(Symbol symbol) {
-    // TODO: complete this once the output format is finalized.
-    return "";
+  public static String serializeSymbol(@Nullable Symbol symbol) {
+    if (symbol == null) {
+      return "null";
+    }
+    switch (symbol.getKind()) {
+      case FIELD:
+      case PARAMETER:
+        return symbol.name.toString();
+      case METHOD:
+      case CONSTRUCTOR:
+        return serializeMethodSignature((Symbol.MethodSymbol) symbol);
+      default:
+        return symbol.flatName().toString();
+    }
+  }
+
+  public static String serializeMethodSignature(Symbol.MethodSymbol methodSymbol) {
+    StringBuilder sb = new StringBuilder();
+    if (methodSymbol.isConstructor()) {
+      // For constructors, method's simple name is <init> and not the enclosing class, need to
+      // locate the enclosing class.
+      Symbol.ClassSymbol encClass = methodSymbol.owner.enclClass();
+      Name name = encClass.getSimpleName();
+      if (name.isEmpty()) {
+        // An anonymous class cannot declare its own constructor. Based on this assumption and our
+        // use case, we should not serialize the method signature.
+        throw new RuntimeException(
+            "Did not expect method serialization for anonymous class constructor: "
+                + methodSymbol
+                + ", in anonymous class: "
+                + encClass);
+      }
+      sb.append(name);
+    } else {
+      // For methods, we use the name of the method.
+      sb.append(methodSymbol.getSimpleName());
+    }
+    sb.append(
+        methodSymbol.getParameters().stream()
+            .map(
+                parameter ->
+                    // check if array
+                    (parameter.type instanceof Type.ArrayType)
+                        // if is array, get the element type and append "[]"
+                        ? ((Type.ArrayType) parameter.type).elemtype.tsym + "[]"
+                        // else, just get the type
+                        : parameter.type.tsym.toString())
+            .collect(joining(",", "(", ")")));
+    return sb.toString();
   }
 }
