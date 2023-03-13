3e6c6f4dd0087a01c07c78164683cf9387938ad6
nimakarimipour <karimipour.nima@gmail.com>
2023-03-13 10:06:05 -0500

    update

--- src/main/java/edu/ucr/cs/riple/taint/ucrtainting/serialization/location/LocalVariableLocation.java
+++ src/main/java/edu/ucr/cs/riple/taint/ucrtainting/serialization/location/LocalVariableLocation.java
@@ -1,11 +1,10 @@
 package edu.ucr.cs.riple.taint.ucrtainting.serialization.location;
 
 import com.sun.tools.javac.code.Symbol;
-
 import javax.lang.model.element.ElementKind;
 
 public class LocalVariableLocation extends AbstractSymbolLocation {
-    public LocalVariableLocation(Symbol target) {
-        super(ElementKind.LOCAL_VARIABLE, target);
-    }
+  public LocalVariableLocation(Symbol target) {
+    super(ElementKind.LOCAL_VARIABLE, target);
+  }
 }
