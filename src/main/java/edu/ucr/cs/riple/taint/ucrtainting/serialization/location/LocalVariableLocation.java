dc4b903ee9a6bd6ac2f534bc0301f34332c7034c
nimakarimipour <karimipour.nima@gmail.com>
2023-03-13 09:40:40 -0500

    add local variable kind

---
+++ src/main/java/edu/ucr/cs/riple/taint/ucrtainting/serialization/location/LocalVariableLocation.java
@@ -0,0 +1,11 @@
+package edu.ucr.cs.riple.taint.ucrtainting.serialization.location;
+
+import com.sun.tools.javac.code.Symbol;
+
+import javax.lang.model.element.ElementKind;
+
+public class LocalVariableLocation extends AbstractSymbolLocation {
+    public LocalVariableLocation(Symbol target) {
+        super(ElementKind.LOCAL_VARIABLE, target);
+    }
+}
