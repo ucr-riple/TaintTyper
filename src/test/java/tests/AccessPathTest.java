d5f7a4e5189d3a1ebc493564ea9dc9acfc0fd1ed
nimakarimipour <karimipour.nima@gmail.com>
2023-03-14 10:14:35 -0500

    add unit tests

---
+++ src/test/java/tests/AccessPathTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import edu.ucr.cs.riple.taint.ucrtainting.UCRTaintingChecker;
+import java.io.File;
+import java.util.List;
+import org.checkerframework.framework.test.CheckerFrameworkPerDirectoryTest;
+import org.junit.runners.Parameterized;
+
+/**
+ * Test runner for tests of the UCR Tainting Checker.
+ *
+ * <p>Tests appear as Java files in the {@code tests/ucrtainting} folder. To add a new test case,
+ * create a Java file in that directory. The file contains "// ::" comments to indicate expected
+ * errors and warnings; see
+ * https://github.com/typetools/checker-framework/blob/master/checker/tests/README .
+ */
+public class AccessPathTest extends CheckerFrameworkPerDirectoryTest {
+    public AccessPathTest(List<File> testFiles) {
+        super(
+                testFiles,
+                UCRTaintingChecker.class,
+                "ucrtainting",
+                "-Anomsgtext",
+                "-Astubs=stubs/",
+                "-nowarn");
+    }
+
+    @Parameterized.Parameters
+    public static String[] getTestDirs() {
+        return new String[] {"accesspath"};
+    }
+}
---
+++ tests/accesspath/Test.java
@@ -0,0 +1,19 @@
+import edu.ucr.cs.riple.taint.ucrtainting.qual.*;
+import java.util.*;
+
+public class Test{
+
+    public HashMap<String, String> foo;
+    Inner inner;
+
+    void bar(){
+        // :: error: assignment
+        @RUntainted String a = foo.keySet().iterator().next();
+        // :: error: assignment
+        @RUntainted String b = inner.bar.keySet().iterator().next();
+    }
+
+    class Inner{
+        public HashMap<String, String> bar;
+    }
+}
