d4425179b3bcd50fad31dd1e360464fd4d3d958d
Kanak Das <kdas006@ucr.edu>
2023-05-16 14:49:54 -0700

    defaulting for runtainted

--- src/main/java/edu/ucr/cs/riple/taint/ucrtainting/qual/RUntainted.java
+++ src/main/java/edu/ucr/cs/riple/taint/ucrtainting/qual/RUntainted.java
@@ -5,9 +5,8 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import org.checkerframework.framework.qual.DefaultFor;
-import org.checkerframework.framework.qual.SubtypeOf;
-import org.checkerframework.framework.qual.TypeUseLocation;
+
+import org.checkerframework.framework.qual.*;
 
 /**
  * Denotes a reference that is untainted, i.e. can be trusted.
@@ -18,5 +17,6 @@ import org.checkerframework.framework.qual.TypeUseLocation;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 @SubtypeOf(RTainted.class)
+@QualifierForLiterals(LiteralKind.STRING)
 @DefaultFor(TypeUseLocation.LOWER_BOUND)
 public @interface RUntainted {}
