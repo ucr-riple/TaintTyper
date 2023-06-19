49a93075888554c52eaa265e59ff4039962b972a
Nima Karimipour <karimipour.nima@gmail.com>
2023-06-19 15:37:31 -0700

    add javadoc

--- src/main/java/edu/ucr/cs/riple/taint/ucrtainting/Config.java
+++ src/main/java/edu/ucr/cs/riple/taint/ucrtainting/Config.java
@@ -6,10 +6,19 @@ import java.nio.file.Paths;
 /** Configuration class for the UCR Tainting project. */
 public class Config {
 
+  /** Flag to specify the output directory among the flags passed to checker framework. */
   public static final String OUTPUT_DIR_FLAG = "outputDir";
+  /**
+   * Flag to specify whether serialization should be enabled or not among the flags passed to
+   * checker framework.
+   */
   public static final String SERIALIZATION_ACTIVATION_FLAG = "enableSerialization";
-
+  /**
+   * The directory where all output files will be written. If not specified, defaults to
+   * /tmp/ucr-tainting/0.
+   */
   public final Path outputDir;
+  /** Flag to control serialization internally. */
   public final boolean serializationActivation;
 
   public Config(UCRTaintingChecker checker) {
@@ -21,6 +30,11 @@ public class Config {
     }
   }
 
+  /**
+   * Returns whether serialization is enabled or not.
+   *
+   * @return true if serialization is enabled, false otherwise.
+   */
   public boolean serializationEnabled() {
     return serializationActivation;
   }
