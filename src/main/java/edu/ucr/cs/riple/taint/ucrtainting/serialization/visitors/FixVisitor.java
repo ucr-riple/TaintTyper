77a808a29811466b4aa271d2c1ac6656e6e0be7e
Nima Karimipour <karimipour.nima@gmail.com>
2023-07-12 16:12:17 -0700

    update

--- src/main/java/edu/ucr/cs/riple/taint/ucrtainting/serialization/visitors/FixVisitor.java
+++ src/main/java/edu/ucr/cs/riple/taint/ucrtainting/serialization/visitors/FixVisitor.java
@@ -12,8 +12,11 @@ import edu.ucr.cs.riple.taint.ucrtainting.FoundRequired;
 import edu.ucr.cs.riple.taint.ucrtainting.UCRTaintingAnnotatedTypeFactory;
 import edu.ucr.cs.riple.taint.ucrtainting.serialization.Fix;
 import edu.ucr.cs.riple.taint.ucrtainting.serialization.Utility;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import javax.lang.model.element.Element;
+import org.checkerframework.framework.type.AnnotatedTypeMirror;
 import org.checkerframework.javacutil.TreeUtils;
 
 /** Generates the fixes for the given tree involved in the reporting error if such fixes exists. */
@@ -69,6 +72,20 @@ public class FixVisitor extends SimpleTreeVisitor<Set<Fix>, Void> {
     boolean isTypeVar = calledMethod.getReturnType() instanceof Type.TypeVar;
     boolean hasReceiver =
         !(calledMethod.isStatic() || receiver == null || Utility.isThisIdentifier(receiver));
+    boolean methodHasTypeArgs = !calledMethod.getTypeParameters().isEmpty();
+    if (methodHasTypeArgs) {
+      Set<Type.TypeVar> effectiveTypes = checkMethodTypeVarImpact(calledMethod, pair);
+      if (!effectiveTypes.isEmpty()) {
+        System.out.println("Method has type args");
+        for (int i = 0; i < node.getArguments().size(); i++) {
+          ExpressionTree passedArg = node.getArguments().get(i);
+          Symbol.VarSymbol paramSymbol = calledMethod.getParameters().get(i);
+          AnnotatedTypeMirror requiredParam =
+              AnnotatedTypeMirror.createType(paramSymbol.type, typeFactory, true);
+          System.out.println("EXCITING");
+        }
+      }
+    }
     // check if the call is to a method defined in a third party library. If the method has a type
     // var return type and has a receiver, we should annotate the receiver.
     if (!isInAnnotatedPackage && !(isTypeVar && hasReceiver)) {
@@ -82,5 +99,60 @@ public class FixVisitor extends SimpleTreeVisitor<Set<Fix>, Void> {
     } else {
       return defaultAction(node, unused);
     }
+  }
+
+  private Set<Type.TypeVar> checkMethodTypeVarImpact(
+      Symbol.MethodSymbol calledMethod, FoundRequired pair) {
+    Set<Type.TypeVar> effectiveTypeVars = new HashSet<>();
+    List<Symbol.TypeVariableSymbol> methodTypeVars = calledMethod.getTypeParameters();
+    for (Symbol.TypeVariableSymbol methodTypeVar : methodTypeVars) {
+      if (typeVarCanImpactFoundRequiredPair(
+          (Type.TypeVar) methodTypeVar.type,
+          calledMethod.getReturnType(),
+          pair.found,
+          pair.required)) {
+        effectiveTypeVars.add((Type.TypeVar) methodTypeVar.type);
+      }
+    }
+    return effectiveTypeVars;
+  }
+
+  /**
+   * Checks if annotating the given type variable on the given type, can make the found type closer
+   * to the required type.
+   *
+   * @param var The type variable to check.
+   * @param type The type to check.
+   * @param found The found type.
+   * @param required The required type.
+   * @return True if annotating the given type variable on the given type, can make the found type
+   *     closer to the required type.
+   */
+  private boolean typeVarCanImpactFoundRequiredPair(
+      Type.TypeVar var, Type type, AnnotatedTypeMirror found, AnnotatedTypeMirror required) {
+    if (type instanceof Type.TypeVar) {
+      return type.equals(var)
+          && typeFactory.hasUntaintedAnnotation(required)
+          && !typeFactory.hasUntaintedAnnotation(found);
+    }
+    if (type instanceof Type.ClassType) {
+      AnnotatedTypeMirror.AnnotatedDeclaredType foundDeclared =
+          (AnnotatedTypeMirror.AnnotatedDeclaredType) found;
+      AnnotatedTypeMirror.AnnotatedDeclaredType requiredDeclared =
+          (AnnotatedTypeMirror.AnnotatedDeclaredType) required;
+      Type.ClassType classType = (Type.ClassType) type;
+      for (int i = 0; i < foundDeclared.getTypeArguments().size(); i++) {
+        boolean canImpact =
+            typeVarCanImpactFoundRequiredPair(
+                var,
+                classType.getTypeArguments().get(i),
+                foundDeclared.getTypeArguments().get(i),
+                requiredDeclared.getTypeArguments().get(i));
+        if (canImpact) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 }
