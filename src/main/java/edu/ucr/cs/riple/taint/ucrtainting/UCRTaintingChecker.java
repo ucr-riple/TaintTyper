29c2deedb5a1898698a3beb2b749c4610b4eafac
Nima Karimipour <karimipour.nima@gmail.com>
2023-08-04 10:09:47 -0700

    silence overriding errors

--- src/main/java/edu/ucr/cs/riple/taint/ucrtainting/UCRTaintingChecker.java
+++ src/main/java/edu/ucr/cs/riple/taint/ucrtainting/UCRTaintingChecker.java
@@ -2,13 +2,20 @@ package edu.ucr.cs.riple.taint.ucrtainting;
 
 import static edu.ucr.cs.riple.taint.ucrtainting.Log.print;
 
+import com.sun.source.tree.Tree;
+import com.sun.tools.javac.code.Symbol;
+import com.sun.tools.javac.code.Types;
+import com.sun.tools.javac.processing.JavacProcessingEnvironment;
 import com.sun.tools.javac.tree.JCTree;
 import edu.ucr.cs.riple.taint.ucrtainting.serialization.SerializationService;
+import edu.ucr.cs.riple.taint.ucrtainting.serialization.Utility;
+import javax.lang.model.element.Element;
 import org.checkerframework.checker.compilermsgs.qual.CompilerMessageKey;
 import org.checkerframework.common.accumulation.AccumulationChecker;
 import org.checkerframework.framework.qual.StubFiles;
 import org.checkerframework.framework.source.SupportedOptions;
 import org.checkerframework.framework.type.AnnotatedTypeMirror;
+import org.checkerframework.javacutil.TreeUtils;
 
 /** This is the entry point for pluggable type-checking. */
 @StubFiles({
@@ -43,6 +50,8 @@ public class UCRTaintingChecker extends AccumulationChecker {
   /** Serialization service for the checker. */
   private SerializationService serializationService;
 
+  private Types types;
+  private UCRTaintingAnnotatedTypeFactory typeFactory;
   private boolean serialize = true;
 
   public UCRTaintingChecker() {}
@@ -51,10 +60,16 @@ public class UCRTaintingChecker extends AccumulationChecker {
   public void initChecker() {
     super.initChecker();
     this.serializationService = new SerializationService(this);
+    this.typeFactory = (UCRTaintingAnnotatedTypeFactory) getTypeFactory();
+    this.types =
+        Types.instance(((JavacProcessingEnvironment) getProcessingEnvironment()).getContext());
   }
 
   @Override
   public void reportError(Object source, @CompilerMessageKey String messageKey, Object... args) {
+    if (shouldBeSkipped(source, messageKey)) {
+      return;
+    }
     print("Index: " + ++index);
     super.reportError(source, messageKey, args);
     if (serialize) {
@@ -69,9 +84,12 @@ public class UCRTaintingChecker extends AccumulationChecker {
 
   public void detailedReportError(
       Object source, @CompilerMessageKey String messageKey, FoundRequired pair, Object... args) {
+    if (shouldBeSkipped(source, messageKey)) {
+      return;
+    }
     this.serializationService.serializeError(source, messageKey, pair);
     this.serialize = false;
-    reportError(source, messageKey, args);
+    this.reportError(source, messageKey, args);
     this.serialize = true;
   }
 
@@ -105,6 +123,35 @@ public class UCRTaintingChecker extends AccumulationChecker {
         }
       default:
         return null;
+    }
+  }
+
+  private boolean shouldBeSkipped(Object source, String messageKey) {
+    Tree tree = (Tree) source;
+    switch (messageKey) {
+      case "override.return":
+        {
+          Symbol.MethodSymbol overridingMethod =
+              (Symbol.MethodSymbol) TreeUtils.elementFromTree(visitor.getCurrentPath().getLeaf());
+          return overridingMethod == null || typeFactory.isInThirdPartyCode(overridingMethod);
+        }
+      case "override.param":
+        {
+          Element treeElement = TreeUtils.elementFromTree(tree);
+          if (treeElement == null) {
+            return true;
+          }
+          Symbol.MethodSymbol overridingMethod =
+              (Symbol.MethodSymbol) treeElement.getEnclosingElement();
+          if (overridingMethod == null) {
+            return true;
+          }
+          Symbol.MethodSymbol overriddenMethod =
+              Utility.getClosestOverriddenMethod(overridingMethod, types);
+          return overriddenMethod == null || typeFactory.isInThirdPartyCode(overriddenMethod);
+        }
+      default:
+        return false;
     }
   }
 }
