da68f6248e05ba890785f0215382fa53c2d03785
Kanak Das <kdas006@ucr.edu>
2023-08-16 14:26:48 -0600

    add iterator and enum test

---
+++ tests/ucrtainting/javaUtilTest/IteratorForEach.java
@@ -0,0 +1,15 @@
+package javaUtilTest;
+
+import java.util.function.Consumer;
+import java.util.Iterator;
+
+class IteratorForEach<T> {
+    private final Iterator<? extends T> it;
+    public IteratorForEach(Iterator<? extends T> iterator) {
+        this.it = iterator;
+    }
+
+    public void test(Consumer<? super T> action) {
+        it.forEachRemaining(action);
+    }
+}
