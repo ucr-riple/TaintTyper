23cc262663bcb38a661b51e6e4faebd3b77656ab
nimakarimipour <karimipour.nima@gmail.com>
2023-09-07 22:46:56 -0700

    add check for poly taint

--- tests/polytaintserialization/foo/bar/Test.java
+++ tests/polytaintserialization/foo/bar/Test.java
@@ -6,44 +6,44 @@ import javax.servlet.http.*;
 
 public class Test {
 
-  //  @RPolyTainted
-  //  String foo(@RPolyTainted String s0, String s1) {
-  //    return s0;
-  //  }
-  //
-  //  protected @RUntainted String contentType = "text/plain";
-  //
-  //  public void simple(String param) {
-  //    @RUntainted String s = foo("foo", param);
-  //    // :: error: assignment
-  //    @RUntainted String s2 = foo(param, "bar");
-  //  }
-  //
-  //  public @RPolyTainted String thirdParty(@RPolyTainted int index, @RPolyTainted String s) {
-  //    return s.substring(index);
-  //  }
-  //
-  //  public void test(HttpServletResponse oResponse, ActionInvocation invocation) {
-  //    oResponse.setContentType(conditionalParse(contentType, invocation));
-  //  }
-  //
-  //  protected @RPolyTainted String conditionalParse(
-  //      @RPolyTainted String param, ActionInvocation invocation) {
-  //    return param;
-  //  }
-  //
-  //  public String inferPoly(String param) {
-  //    String s = param;
-  //    return s;
-  //  }
-  //
-  //  public void test() {
-  //    // :: error: assignment
-  //    @RUntainted String s = inferPoly("foo");
-  //  }
-  //
-  //  class ActionInvocation {}
-  //
+  @RPolyTainted
+  String foo(@RPolyTainted String s0, String s1) {
+    return s0;
+  }
+
+  protected @RUntainted String contentType = "text/plain";
+
+  public void simple(String param) {
+    @RUntainted String s = foo("foo", param);
+    // :: error: assignment
+    @RUntainted String s2 = foo(param, "bar");
+  }
+
+  public @RPolyTainted String thirdParty(@RPolyTainted int index, @RPolyTainted String s) {
+    return s.substring(index);
+  }
+
+  public void test(HttpServletResponse oResponse, ActionInvocation invocation) {
+    oResponse.setContentType(conditionalParse(contentType, invocation));
+  }
+
+  protected @RPolyTainted String conditionalParse(
+      @RPolyTainted String param, ActionInvocation invocation) {
+    return param;
+  }
+
+  public String inferPoly(String param) {
+    String s = param;
+    return s;
+  }
+
+  public void test() {
+    // :: error: assignment
+    @RUntainted String s = inferPoly("foo");
+  }
+
+  class ActionInvocation {}
+
   public String inferPolyWithTypeParamAnnotation(List<String> param) {
     String s = param.get(0);
     return s;
