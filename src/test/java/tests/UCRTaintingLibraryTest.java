aa202373ad826ec2a8bb1cf579318752468e211f
Kanak Das <kdas006@ucr.edu>
2024-01-10 22:20:47 -0800

    FP in ESAPI

--- src/test/java/tests/UCRTaintingLibraryTest.java
+++ src/test/java/tests/UCRTaintingLibraryTest.java
@@ -1,8 +1,9 @@
 import edu.ucr.cs.riple.taint.ucrtainting.UCRTaintingChecker;
-import java.io.File;
-import java.util.List;
 import org.checkerframework.framework.test.CheckerFrameworkPerDirectoryTest;
 import org.junit.runners.Parameterized.Parameters;
+
+import java.io.File;
+import java.util.List;
 
 /**
  * Test runner for tests of the UCR Tainting Checker.
@@ -35,7 +36,8 @@ public class UCRTaintingLibraryTest extends CheckerFrameworkPerDirectoryTest {
       "ucrtainting/stringBuilderTests",
       "ucrtainting/thirdPartyTests",
       "ucrtainting/springSecOAuth",
-      "ucrtainting/crashTests"
+      "ucrtainting/crashTests",
+      "ucrtainting/esapiNullFieldTest"
     };
   }
 }
---
+++ tests/ucrtainting/esapiNullFieldTest/EsapiNullFieldTest.java
@@ -0,0 +1,17 @@
+package esapiNullFieldTest;
+
+import edu.ucr.cs.riple.taint.ucrtainting.qual.RUntainted;
+
+public class EsapiNullFieldTest {
+    public String fieldData = null;
+
+    public void setter(String s) {
+        this.fieldData = s;
+    }
+    public @RUntainted String source() {
+        return this.fieldData;
+    }
+
+    public void sink(@RUntainted String data) {}
+
+}
