04a0d227604b4c76e476f40846485399d1a3bc52
nimakarimipour <karimipour.nima@gmail.com>
2024-04-03 00:44:01 -0700

    update

--- src/main/java/edu/ucr/cs/riple/taint/ucrtainting/serialization/visitors/TypeMatchVisitor.java
+++ src/main/java/edu/ucr/cs/riple/taint/ucrtainting/serialization/visitors/TypeMatchVisitor.java
@@ -58,6 +58,11 @@ public class TypeMatchVisitor extends AbstractAtmComboVisitor<List<List<Integer>
   }
 
   @Override
+  public List<List<Integer>> visitWildcard_Typevar(AnnotatedTypeMirror.AnnotatedWildcardType type1, AnnotatedTypeMirror.AnnotatedTypeVariable type2, Void unused) {
+    return supportedDefault(type1.getExtendsBound(), type2);
+  }
+
+  @Override
   public List<List<Integer>> visitDeclared_Primitive(
       AnnotatedTypeMirror.AnnotatedDeclaredType type1,
       AnnotatedTypeMirror.AnnotatedPrimitiveType type2,
