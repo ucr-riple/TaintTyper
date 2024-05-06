c4c58368ebea0c460f72bd6d6a6b28c4b35f5a22
Nima Karimipour <karimipour.nima@gmail.com>
2024-05-06 15:54:11 -0700

    add javadoc to TypeIndex

--- src/main/java/edu/ucr/cs/riple/taint/ucrtainting/serialization/TypeIndex.java
+++ src/main/java/edu/ucr/cs/riple/taint/ucrtainting/serialization/TypeIndex.java
@@ -7,10 +7,13 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
+/** Represents a type index in a type hierarchy. */
 public class TypeIndex {
 
+  /** List of indices to the target type. */
   private final List<Integer> content;
 
+  /** Top level type index. This index will target the top level type in the hierarchy. */
   public static final TypeIndex TOP_LEVEL = TypeIndex.of(0);
 
   public TypeIndex() {
@@ -21,6 +24,12 @@ public class TypeIndex {
     this.content = new ArrayList<>(other.content);
   }
 
+  /**
+   * Creates a new type index with the given indexes.
+   *
+   * @param indexes indexes to the target type
+   * @return a new type index
+   */
   public static TypeIndex of(int... indexes) {
     TypeIndex typeIndex = new TypeIndex();
     for (int index : indexes) {
@@ -29,28 +38,58 @@ public class TypeIndex {
     return typeIndex;
   }
 
+  /**
+   * Creates a new type index with the given indexes.
+   *
+   * @param indexes indexes to the target type
+   * @return a new type index
+   */
   public static TypeIndex of(List<Integer> indexes) {
     TypeIndex typeIndex = new TypeIndex();
     typeIndex.content.addAll(indexes);
     return typeIndex;
   }
 
+  /**
+   * Creates a new set of type indexes with the given indexes.
+   *
+   * @param indexes indexes to the target type
+   * @return a new set of type indexes
+   */
   public static Set<TypeIndex> setOf(int... indexes) {
     Set<TypeIndex> set = new HashSet<>();
     set.add(TypeIndex.of(indexes));
     return set;
   }
 
+  /**
+   * Creates a new set of type indexes with the given type index.
+   *
+   * @param index type index
+   * @return a new set of type indexes
+   */
   public static Set<TypeIndex> setOf(TypeIndex index) {
     Set<TypeIndex> set = new HashSet<>();
     set.add(index);
     return set;
   }
 
+  /**
+   * Returns the top level type index.
+   *
+   * @return the top level type index
+   */
   public static Set<TypeIndex> topLevel() {
     return setOf(TOP_LEVEL);
   }
 
+  /**
+   * Creates a new type index which is relative to the given type index. (e.g. given type index is
+   * [2, 1] and this type index is [3, 4], the relative type index will be [2, 1, 3, 4])
+   *
+   * @param other Type index to be relative to
+   * @return a new type index which is relative to the given type index
+   */
   public TypeIndex relativeTo(TypeIndex other) {
     TypeIndex relative = new TypeIndex();
     relative.content.addAll(other.content);
@@ -58,26 +97,57 @@ public class TypeIndex {
     return relative;
   }
 
+  /**
+   * Checks if this type index is empty.
+   *
+   * @return true if this type index is empty, false otherwise
+   */
   public boolean isEmpty() {
     return content.isEmpty();
   }
 
+  /**
+   * Removes the first index from the type index and returns it.
+   *
+   * @return the first index
+   */
   public Integer poll() {
     return content.remove(0);
   }
 
+  /**
+   * Returns the size of the type index.
+   *
+   * @return the size of the type index
+   */
   public int size() {
     return content.size();
   }
 
+  /**
+   * Returns the index at the given position.
+   *
+   * @param index position of the index
+   * @return the index at the given position
+   */
   public Integer get(int index) {
     return content.get(index);
   }
 
+  /**
+   * Returns the content of the type index.
+   *
+   * @return ImmutableList of the content of the type index.
+   */
   public ImmutableList<Integer> getContent() {
     return ImmutableList.copyOf(content);
   }
 
+  /**
+   * Returns a copy of this type index.
+   *
+   * @return a copy of this type index
+   */
   public TypeIndex copy() {
     return new TypeIndex(this);
   }
