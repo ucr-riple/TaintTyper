d3a9493be2959914642b4534949ac3c3c93b60e3
Nima Karimipour <karimipour.nima@gmail.com>
2024-05-08 20:32:47 -0700

    add test

--- tests/templatetest/foo/bar/Test.java
+++ tests/templatetest/foo/bar/Test.java
@@ -3,4 +3,17 @@ package foo.bar;
 import edu.ucr.cs.riple.taint.ucrtainting.qual.*;
 import java.util.*;
 
-public class Test {}
+public class Test {
+
+    public void test(Foo<String, String> foo){
+        // :: error: enhancedfor
+        for(@RUntainted String s : foo){
+
+        }
+    }
+
+
+    class Foo<T, M> extends ArrayList<M>{
+
+    }
+}
