f8a3d81b7b6c6e50674007f7dd545fca5dab3592
nimakarimipour <karimipour.nima@gmail.com>
2024-05-13 00:13:41 -0700

    add hash and equal

--- tests/templatetest/foo/bar/Test.java
+++ tests/templatetest/foo/bar/Test.java
@@ -4,11 +4,4 @@ import edu.ucr.cs.riple.taint.ucrtainting.qual.*;
 import java.lang.annotation.*;
 import java.util.*;
 
-public class Test {
-
-  public void test(ActionInvocation action, List<Method> methods) {
-    for (Method m : methods) {
-      final String resultCode = (String) MethodUtils.invokeMethod(action, true, m.getName());
-    }
-  }
-}
+public class Test {}
