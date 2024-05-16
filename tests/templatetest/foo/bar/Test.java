3624bcf23fc8fb9546eea8d92280a6a066fba2c2
Nima Karimipour <karimipour.nima@gmail.com>
2024-05-16 10:18:05 -0700

    add todo test

--- tests/templatetest/foo/bar/Test.java
+++ tests/templatetest/foo/bar/Test.java
@@ -4,4 +4,20 @@ import edu.ucr.cs.riple.taint.ucrtainting.qual.*;
 import java.lang.annotation.*;
 import java.util.*;
 
-public class Test {}
+public class Test {
+
+//    public void test(Map<Map<List<String>, String>, List<String>> map){
+//        for (Map<List<String>, String> smallMap : map.keySet()) {
+//            for(Map.Entry<List<String>, String> entry : smallMap.entrySet()){
+//                for(String s : entry.getKey()){
+//                    // :: error: argument
+//                    sink(s);
+//                }
+//            }
+//        }
+//    }
+//
+//    void sink(@RUntainted String s){
+//
+//    }
+}
